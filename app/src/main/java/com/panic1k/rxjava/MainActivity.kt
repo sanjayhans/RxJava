9ae94cbc44bf8d38ef2584f106d6b048de1de71a
Sanjay Hans <shans@cricut.com>
2018-09-21 12:07:54 +0530

    init commit

---
+++ app/src/main/java/com/panic1k/rxjava/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.panic1k.rxjava
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
